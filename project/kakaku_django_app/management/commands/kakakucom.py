48d678985eb416cd516fe6c7de3533327cfc7fe9
shira0831 <white.world.0831@gmail.com>
2022-04-24 12:41:21 +0900

    20220424commit

--- project/kakaku_django_app/management/commands/kakakucom.py
+++ project/kakaku_django_app/management/commands/kakakucom.py
@@ -6,34 +6,34 @@ from ..modules import get_pc_data
 from sqlalchemy import create_engine
 import pandas as pd
 
-arr = []
 
 class Command(BaseCommand):
     def add_arguments(self, parser): #コマンド引数の定義
         # int型の必須引数の定義
-        tp = lambda x:list(map(str, x.split(',')))
-        parser.add_argument('id', type=tp, nargs='*')
+        # tp = lambda x:list(map(str, x.split(',')))
+        parser.add_argument('id', type=str, nargs='*')
         args = parser.parse_args()
+        print('っっっっっっっd',args)
+        print('test',args.id[1])
+        output= eval(args.id[1])
+        print('output',output)  
 
-        print('test',args.id[1][0])
-
-        global arr
-        arr.append(args.id[1])
 
     def handle(self, *args,**kwargs):
         self.stdout.write(self.style.SUCCESS('id = "%s"' % kwargs['id']))
-        # driverを生成
         print('argsargsargsargsargsargs',args)
         print('kwargskwargskwargskwargs',kwargs)
-        
+        print('kwargskwargskwargskwargskwargskwargskwargskwargs',kwargs['id'])
+        print('eval',eval(kwargs['id'][0]))
+
+        # driverを生成
         chrome_driver = ChromeDriver()
         # genre_urls = [
-        #     'https://kakaku.com/used/pc/ca=0030/',
-        #     'https://kakaku.com/used/pc/ca=0080/',
-        #     'https://kakaku.com/used/pc/ca=0019/',
-        #     'https://kakaku.com/used/pc/ca=0010/',
-        #     'https://kakaku.com/used/pc/ca=0020/',
-        #     'https://kakaku.com/used/pc/ca=0030/',
+        #     'https://kakaku.com/used/pc/ca=0030/',# 中古タブレットPC
+        #     'https://kakaku.com/used/pc/ca=0080/',# 中古モニタ・ディスプレイ 
+        #     'https://kakaku.com/used/pc/ca=0019/',# 中古Mac デスクトップパソコン
+        #     'https://kakaku.com/used/pc/ca=0010/',# 中古デスクトップパソコン
+        #     'https://kakaku.com/used/pc/ca=0020/',# 中古ノートパソコン
         # ]
 
         # genre_urls = [
@@ -44,48 +44,39 @@ class Command(BaseCommand):
         # 'https://kakaku.com/pc/pda/itemlist.aspx'# すべて タブレットPC
         # ]
 
-        test_urls = [
-        'https://kakaku.com/used/pc/ca=0030/',
-        'https://kakaku.com/used/pc/ca=0080/',
-        'https://kakaku.com/used/pc/ca=0019/',
-        'https://kakaku.com/used/pc/ca=0010/',
-        'https://kakaku.com/used/pc/ca=0020/',
-        'https://kakaku.com/used/pc/ca=0030/',
-        'https://kakaku.com/pc/note-pc/itemlist.aspx', # すべて ノートパソコン
-        'https://kakaku.com/pc/desktop-pc/itemlist.aspx',# すべて デスクトップパソコン
-        'https://kakaku.com/pc/mac-note-pc/itemlist.aspx',# すべて Mac ノート(MacBook)
-        'https://kakaku.com/pc/mac-desktop-pc/itemlist.aspx',# すべて Mac デスクトップ
-        'https://kakaku.com/pc/pda/itemlist.aspx'# すべて タブレットPC
-        ]
+        # test_urls = [
+        # 'https://kakaku.com/used/pc/ca=0030/',
+        # 'https://kakaku.com/used/pc/ca=0080/',
+        # 'https://kakaku.com/used/pc/ca=0019/',
+        # 'https://kakaku.com/used/pc/ca=0010/',
+        # 'https://kakaku.com/used/pc/ca=0020/',
+        # 'https://kakaku.com/pc/note-pc/itemlist.aspx', # すべて ノートパソコン
+        # 'https://kakaku.com/pc/desktop-pc/itemlist.aspx',# すべて デスクトップパソコン
+        # 'https://kakaku.com/pc/mac-note-pc/itemlist.aspx',# すべて Mac ノート(MacBook)
+        # 'https://kakaku.com/pc/mac-desktop-pc/itemlist.aspx',# すべて Mac デスクトップ
+        # 'https://kakaku.com/pc/pda/itemlist.aspx'# すべて タブレットPC
+        # ]
 
-        
-
-        print('7777777',arr[0])
-        genre_urls = []
-        for i in arr[0]:
-            print('urls',i)
-            j =int(i)
-            genre_urls.append(test_urls[j-1])
-
-        print(genre_urls)
-        
+        data= []
+        genre_urls = eval(kwargs['id'][0])
 
         for genre_url in genre_urls:
-
-            data= []
+            print('target_url',genre_url)
+    
+            # 変数セット
+            url = genre_url['url']
+            genre = genre_url['genre']
 
             # ページ数を取得する
-            page_counts = get_pc_data.get_page_counts(chrome_driver, genre_url)
+            page_counts = get_pc_data.get_page_counts(chrome_driver, url, genre)
 
-            # for i in range(1, page_counts + 1):
-            for i in range(1, 2):
-        
+            for i in range(1, page_counts + 1):
+            # for i in range(1, 2):
                 # ページを表示して各ページ内の商品urlを格納
-                links = get_pc_data.get_item_urls(chrome_driver,genre_url,i)
-
+                links = get_pc_data.get_item_urls(chrome_driver, url, genre, i)
                 # 各商品url内の情報を取得
                 for link in links:
-                    result = get_pc_data.get_data(chrome_driver,link)
+                    result = get_pc_data.get_data(chrome_driver, genre, link)
                     data.append(result)
 
         # driverを削除する
--- project/kakaku_django_app/views.py
+++ project/kakaku_django_app/views.py
@@ -3,7 +3,9 @@ import threading
 
 from django.views.generic import View
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+
 
 
 class IndexView(View):
@@ -12,9 +14,21 @@ class IndexView(View):
 
     def post(self, request):
         check1 = request.POST.getlist("used_pc")
-        check2 = request.POST.getlist("newPC")
+        check2 = request.POST.getlist("new_pc")
+        check3 = request.POST.getlist("sp")
         print('こここここ',check1,type(check1))
         print('こここここ',check2)
+
+        d = [{"url":value,'genre':'used_pc'} for value in check1]
+        d2 = [{"url":value,'genre':'new_pc'} for value in check2]
+        d3 = [{"url":value,'genre':'sp'} for value in check3]
+
+        print('dict',d)
+        print('dict2',d2)
+
+        dict = d
+        dict.extend(d2)
+        dict.extend(d3)
 
 
         print('スクレイピングボタンが押されました')
@@ -23,12 +37,15 @@ class IndexView(View):
             """インナー関数としてリサーチスクリプト起動のコマンドを定義"""
             # <アプリ名>/management/commands/<スクリプト名>.py　を作成すると、
             # python manage.py <スクリプト名>でそのスクリプトを実行できます！ (https://docs.djangoproject.com/en/4.0/howto/custom-management-commands/)
-            subprocess.run(["python", "manage.py", "kakakucom",','.join(check1)], check=True)
+            # subprocess.run(["python", "manage.py", "kakakucom",','.join(check1)], check=True)
+            subprocess.run(["python", "manage.py", "kakakucom",str(dict)], check=True)
+
         
         # リサーチスクリプトはバックで起動するためスレッドとして実行
         th = threading.Thread(target=research)
         th.start()
 
         # レスポンスを返す 例：「スタートしました」「失敗しました」等の表示
-        response = HttpResponse('<h1>成功</h1>')
+        # response = HttpResponse('<h1>成功</h1>')
+        response = redirect('/success')
         return response
